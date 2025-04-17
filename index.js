35c5225bd54ee7e5e5facf839a59d74ba11b9638
Carlos-SaulPM <carlossaulpazmaldonado@gmail.com>
2025-04-17 17:46:43 -0600

    Implementando express-fileupload

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 require("dotenv").config();
 const app = express();
@@ -8,18 +7,21 @@ const PORT = process.env.PORT;
 const path = require("path");
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
 
-const {peliculaApiRouter} = require("./routers");
+const { peliculaApiRouter } = require("./routers");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use(morgan("tiny"));
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
+app.use(fileUpload());
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
   res.render("index");
-})
+});
 
 app.use("/api", peliculaApiRouter);
 
--- routers/peliculaApi_router.js
+++ routers/peliculaApi_router.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const {peliculaRepository} = require("../repositories")
-const {peliculaModel} = require("../models")
+const { peliculaRepository } = require("../repositories");
+const { peliculaModel } = require("../models");
 const router = express.Router();
 
 router.post("/pelicula/guardar", async (req, res, next) => {
-  peliculaRepository.subirPelicula(peliculaModel)
-
+  peliculaRepository.subirPelicula(
+    peliculaModel.peliculaParaGuardar(req.body.titulo, req.body.sinopsis)
+  );
 });
 
 module.exports = router;
